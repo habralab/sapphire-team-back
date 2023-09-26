58cdbb3bc26423e5e569d3bf8943391b28919864
YehorI <skylainer99@gmail.com>
2023-09-26 15:37:26 +0300

    alembic async, linting, uuid fix, datetime fix

--- sapphire/projects/database/migrations/env.py
+++ sapphire/projects/database/migrations/env.py
@@ -1,7 +1,9 @@
+import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
@@ -16,8 +18,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from sapphire.projects.database import models
-target_metadata = models.Base.metadata
+from sapphire.projects.database.models import Base
+target_metadata = Base.metadata
 # target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
@@ -50,26 +52,35 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
 
-    In this scenario we need to create an Engine
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
+
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
-        with context.begin_transaction():
-            context.run_migrations()
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
--- sapphire/projects/database/models.py
+++ sapphire/projects/database/models.py
@@ -1,55 +1,58 @@
-from sqlalchemy import Column, String, Text, UUID, DateTime, ForeignKey, Null, Enum
+from uuid import uuid4
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey, Enum, UUID
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import func
 
 
 class ProjectStatusEnum(Enum):
     IN_PROGRESS = "in_progress"
     COMPLETED = "completed"
 
+
 class ParticipantStatusEnum(Enum):
     ACTIVE = "active"
     INACTIVE = "inactive"
 
+
 Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(UUID, primary_key=True)
 
 
 class Project(Base):
     __tablename__ = "projects"
 
-    id = Column(UUID, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
     name = Column(String(255), nullable=False)
     description = Column(Text, nullable=True)
     owner_id = Column(UUID, nullable=False)
-    deadline = Column(DateTime, default=Null)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    deadline = Column(DateTime(timezone=True))  # not sure in format
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+
 
 class History(Base):
     __tablename__ = "projects_history"
 
-    id = Column(UUID, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
     project_id = Column(UUID, ForeignKey("projects.id"))
     status = Column(ProjectStatusEnum('ProjectStatusEnum'), nullable=False)
-    created_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
 
 class Position(Base):
     __tablename__ = "project_positions"
 
-    id = Column(UUID, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
+    name = Column(String(255), nullable=False)
     project_id = Column(UUID, ForeignKey("projects.id"))
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+
 
 class Participant(Base):
     __tablename__ = "project_participants"
 
-    id = Column(UUID, primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4())
     position_id = Column(UUID, ForeignKey("project_positions.id"))
-    user_id = Column(UUID, ForeignKey("users.id"))
+    user_id = Column(UUID, nullable=False)
     status = Column(ParticipantStatusEnum('ParticipantStatusEnum'), nullable=False)
-    created_at = Column(DateTime, nullable=False)   
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
