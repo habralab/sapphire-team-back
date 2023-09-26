e106ef73f8fdb1c00f6b79970f1c5a4a73a11ef0
Oleg Yurchik <oleg@yurchik.space>
2023-09-26 09:05:44 +0300

    task/43 Add FastAPI service for Projects App

---
+++ sapphire/projects/api/service.py
@@ -0,0 +1,21 @@
+import fastapi
+
+from sapphire.common.api.service import APIService
+from sapphire.projects.settings import ProjectsSettings
+from sapphire.projects.__version__ import __version__
+from .router import router
+
+
+class ProjectsAPIService(APIService):
+    def __init__(self, version: str, port: int = 8000):
+        super().__init__(title="Projects", version=version, port=port)
+
+    def setup_app(self, app: fastapi.FastAPI):
+        app.include_router(router, prefix="/api")
+
+
+def get_service(settings: ProjectsSettings) -> ProjectsAPIService:
+    return ProjectsAPIService(
+        version=__version__,
+        port=settings.port,
+    )
