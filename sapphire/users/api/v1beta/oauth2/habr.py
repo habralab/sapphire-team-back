b4d5c7efd4b44344546c76bd63203bc0cda86101
brulitsan <124054713+brulitsan@users.noreply.github.com>
2023-09-28 05:35:54 +0300

    Revert "task/27 endpoints authorise, callback"

--- sapphire/users/api/v1beta/oauth2/habr.py
+++ sapphire/users/api/v1beta/oauth2/habr.py
@@ -1,25 +1,17 @@
 import fastapi
 import yarl
 from fastapi.responses import RedirectResponse
-from sapphire.users.oauth2.base import OAuth2BaseBackend
-from sapphire.users.oauth2.habr import get_oauth2_backend, UsersSettings
+
 
 router = fastapi.APIRouter()
 
 
 @router.get("/authorize", response_class=RedirectResponse)
-async def authorize(request: fastapi.Request, client_id: str, client_secret: str):
+async def authorize(request: fastapi.Request):
     redirect_url = yarl.URL(str(request.url)).parent / "callback"
-    backend = OAuth2BaseBackend(client_id=client_id, client_secret=client_secret)
-    authorisation_url = backend.get_authorization_url(redirect_url=str(redirect_url))
-    return RedirectResponse(url=authorisation_url)
+
     # Get OAuth2 backend from request.app.habr_oauth2
 
-
 @router.get("/callback")
-async def callback(client_id: str, client_secret: str, state: str, code: str):
-    settings = UsersSettings(habr_oauth2_client_id=client_id, habr_oauth2_client_secret=client_secret)
-    backend = get_oauth2_backend(settings)
-    token = await backend.get_token(state=state, code=code)
-    user_info = await backend.get_user_info(token=token)
-    return user_info
+async def callback():
+    pass
