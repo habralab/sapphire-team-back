7ec0a4c4d6680d6be6ba3b639be57b867cecc7f6
matve <matveytulaevm.ama@gmail.com>
2023-10-03 13:01:17 +0300

    task 62 modernization#2+pylint fixes+isort

--- tests/common/test_package.py
+++ tests/common/test_package.py
@@ -2,7 +2,7 @@ from unittest.mock import mock_open, patch
 
 import pytest
 
-from sapphire.common.utils.package import get_version
+from sapphire.common.package import get_version
 
 PYPROJECT_CONTENT_TEMPLATE = """
 [tool.poetry]
