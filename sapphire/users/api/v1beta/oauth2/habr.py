9d2cbc0f20a20b1f28407798f97ea9002e2a2d94
Oleg Yurchik <oleg@yurchik.space>
2023-10-04 18:00:40 +0300

    hotfix/27 fix authorization

--- sapphire/users/api/v1beta/oauth2/habr.py
+++ sapphire/users/api/v1beta/oauth2/habr.py
@@ -17,7 +17,9 @@ router = fastapi.APIRouter()
 async def authorize(request: fastapi.Request):
     habr_oauth2: OAuth2HabrBackend = request.app.service.habr_oauth2
 
-    redirect_url = yarl.URL(request.app.extra["root_url"]) / request.app.root_path / "callback"
+    redirect_url = yarl.URL(request.app.extra["root_url"])
+    redirect_url /= request.url.path.lstrip("/")
+    redirect_url = redirect_url.parent / "callback"
     authorization_url = habr_oauth2.get_authorization_url(
         redirect_url=str(redirect_url),
     )
