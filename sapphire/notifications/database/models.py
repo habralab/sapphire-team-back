aec36df559c8640f044fa18ea00fc540475a1afe
Oleg Yurchik <oleg@yurchik.space>
2023-10-04 14:07:35 +0300

    task/67 add database for notifications

---
+++ sapphire/notifications/database/models.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import datetime
+from typing import Any
+
+from sqlalchemy import JSON
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        dict[str, Any]: JSON,
+    }
+
+
+class Notification(Base):
+    __tablename__ = "notifications"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    type: Mapped[str] = mapped_column(nullable=False)
+    recipient_id: Mapped[uuid.UUID] = mapped_column(nullable=False)
+    data: Mapped[dict[str, Any]] = mapped_column(default=dict)
+    is_read: Mapped[bool] = mapped_column(default=False)
+
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
