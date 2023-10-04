8c217c54b32085a8d9a075def5f9f4cf5c6976cf
Oleg Yurchik <oleg@yurchik.space>
2023-10-04 14:07:35 +0300

    task/67 add database for notifications

--- sapphire/notifications/database/migrations/versions/6cb5d755a7b9_init.py
+++ sapphire/notifications/database/migrations/versions/6cb5d755a7b9_init.py
@@ -7,8 +7,9 @@ Create Date: 2023-10-04 14:18:33.835579
 """
 from typing import Sequence, Union
 
-import sqlalchemy as sa
 from alembic import op
+import sqlalchemy as sa
+
 
 # revision identifiers, used by Alembic.
 revision: str = "6cb5d755a7b9"
