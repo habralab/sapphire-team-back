6763271a0e67193193d89696843cbb38d782690e
YehorI <skylainer99@gmail.com>
2023-10-06 15:55:06 +0300

    task/63 isort

--- sapphire/users/api/v1beta/oauth2/habr.py
+++ sapphire/users/api/v1beta/oauth2/habr.py
@@ -7,8 +7,10 @@ from fastapi.responses import RedirectResponse
 from sapphire.common.api.schemas import OKResponse
 from sapphire.common.api.schemas.enums import ResponseStatus
 from sapphire.users.api.schemas import JWTTokensResponse
+from sapphire.users.database.models import User
 from sapphire.users.jwt import JWTMethods
 from sapphire.users.oauth2.habr import OAuth2HabrBackend
+from sapphire.users.service import get_service
 
 router = fastapi.APIRouter()
 
@@ -39,6 +41,17 @@ async def callback(
         raise fastapi.HTTPException(status_code=401, detail="Not authenticated")
 
     user_id = uuid.uuid4()  # temporary solution, refactor will be later
+    database_service = get_service()
+    async with database_service._sessionmaker() as session:
+        user = await habr_oauth2.get_user_info(token)
+        user_in_db = await session.query(User).filter(
+            HabrUser.id == user.id
+        ).first()
+
+        if not user_in_db:
+            new_user_in_db = User(id= user.id, email=user.email, first_name=user.login)
+            session.add(new_user_in_db)
+            await session.commit()
 
     access_token = jwt_methods.issue_access_token(user_id)
     refresh_token = jwt_methods.issue_refresh_token(user_id)
