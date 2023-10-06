55dff69bc888675720d325ce7008295ce80f2559
YehorI <skylainer99@gmail.com>
2023-10-06 17:42:45 +0300

    task/63 UserDatabaseService create_user

--- sapphire/users/api/v1beta/oauth2/habr.py
+++ sapphire/users/api/v1beta/oauth2/habr.py
@@ -10,7 +10,7 @@ from sapphire.users.api.schemas import JWTTokensResponse
 from sapphire.users.database.models import User
 from sapphire.users.jwt import JWTMethods
 from sapphire.users.oauth2.habr import OAuth2HabrBackend
-from sapphire.users.service import get_service
+from sapphire.users.service import UserDatabaseService
 
 router = fastapi.APIRouter()
 
@@ -35,23 +35,14 @@ async def callback(
 ) -> JWTTokensResponse:
     habr_oauth2: OAuth2HabrBackend = request.app.service.habr_oauth2
     jwt_methods: JWTMethods = request.app.service.jwt_methods
+    database_service: UserDatabaseService = request.app.service.database
 
     token = await habr_oauth2.get_token(state, code)
     if token is None:
         raise fastapi.HTTPException(status_code=401, detail="Not authenticated")
 
-    user_id = uuid.uuid4()  # temporary solution, refactor will be later
-    database_service = get_service()
-    async with database_service._sessionmaker() as session:
-        user = await habr_oauth2.get_user_info(token)
-        user_in_db = await session.query(User).filter(
-            User.id == user.id
-        ).first()
-
-        if not user_in_db:
-            new_user_in_db = User(id= user.id, email=user.email, first_name=user.login)
-            session.add(new_user_in_db)
-            await session.commit()
+    user_info = await habr_oauth2.get_user_info(token)
+    await database_service.create_user(user_info)
 
     access_token = jwt_methods.issue_access_token(user_id)
     refresh_token = jwt_methods.issue_refresh_token(user_id)
--- sapphire/users/database/service.py
+++ sapphire/users/database/service.py
@@ -8,6 +8,20 @@ class UsersDatabaseService(BaseDatabaseService):
     def get_alembic_config_path(self) -> pathlib.Path:
         return pathlib.Path(__file__).parent / "migrations"
 
+    async def create_user(self, user_info):
+        async with self.database_service._sessionmaker() as session:
+            user_in_db = await session.query(User).filter(
+                User.email == user_info.email
+            ).first()
+            if not user_in_db:
+                user = User(
+                    id=user_info.id,
+                    email=user_info.email,
+                    first_name=user_info.login,
+                )
+                session.add(user)
+            await session.commit()
+
 
 def get_service(settings: UsersSettings) -> UsersDatabaseService:
     return UsersDatabaseService(dsn=str(settings.db_dsn))
