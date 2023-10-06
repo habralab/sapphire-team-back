822ad4429a8df3a17126e0baecab2b77936257fc
matve <matveytulaevm.ama@gmail.com>
2023-10-06 20:21:09 +0300

    task 83 create models for messenger app + linters

--- sapphire/messenger/database/migrations/versions/a646896fd0ba_.py
+++ sapphire/messenger/database/migrations/versions/a646896fd0ba_.py
@@ -7,8 +7,8 @@ Create Date: 2023-10-06 20:10:56.017374
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'a646896fd0ba'
