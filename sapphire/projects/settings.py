54ddaa65cdd5c337c0063a336b267dd720ca08a2
Oleg Yurchik <oleg@yurchik.space>
2023-10-08 23:30:58 +0300

    hotfix/91 add jwtsettings to users and projects settings

--- sapphire/projects/settings.py
+++ sapphire/projects/settings.py
@@ -1,11 +1,12 @@
 from pydantic import AnyUrl
 from pydantic_settings import SettingsConfigDict
 
+from sapphire.common.api.jwt.settings import JWTSettings
 from sapphire.common.api.settings import BaseAPISettings
 from sapphire.common.database.settings import BaseDatabaseSettings
 
 
-class ProjectsSettings(BaseAPISettings, BaseDatabaseSettings):
+class ProjectsSettings(BaseAPISettings, BaseDatabaseSettings, JWTSettings):
     model_config = SettingsConfigDict()
 
     db_dsn: AnyUrl = AnyUrl("sqlite+aiosqlite:///projects.sqlite3")
