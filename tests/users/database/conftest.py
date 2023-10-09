4c7ec801295979e9944be4dacdb8623cbee01493
YehorI <skylainer99@gmail.com>
2023-10-09 20:49:36 +0300

    task/63 Tests for get/create user

--- tests/users/database/conftest.py
+++ tests/users/database/conftest.py
@@ -7,3 +7,15 @@ from sapphire.users.settings import UsersSettings
 @pytest.fixture()
 def database_service(settings: UsersSettings) -> UsersDatabaseService:
     return UsersDatabaseService(dsn=str(settings.db_dsn))
+
+@pytest.fixture()
+def settings():
+    return UsersSettings()
+
+@pytest.fixture
+async def async_session():
+    session = AsyncSession()
+    try:
+        yield session
+    finally:
+        await session.close()
--- tests/users/database/test_users_database_service.py
+++ tests/users/database/test_users_database_service.py
@@ -1,7 +1,15 @@
 import os
 import pathlib
+import uuid
+
+from unittest.mock import MagicMock, AsyncMock
+
+import pytest
 
 from sapphire.users.database.service import UsersDatabaseService
+from sapphire.users.database.models import User
+
+AsyncSession = []
 
 
 def test_get_alembic_config_path(database_service: UsersDatabaseService):
@@ -15,5 +23,64 @@ def test_get_alembic_config_path(database_service: UsersDatabaseService):
     assert path == expected_path
 
 
-def test_get_or_create_user(database_service: UsersDatabaseService):
-    ...
+@pytest.mark.asyncio
+async def test_get_user(database_service: UsersDatabaseService):
+    session = MagicMock()
+    user_id = uuid.uuid4()
+    email = "test@gmail.com"
+
+    # Create a mock User object
+    mock_user = MagicMock()
+    mock_user.email = email
+
+    # Mock the session.query().filter().first() chain to return the mock User object
+    session().query().filter().first.return_value = mock_user
+
+    user = await database_service.create_user(
+        session=session,
+        user_id=user_id,
+        email=email,
+    )
+
+    got_user = await database_service.get_user(
+        session=session,
+        email=email,
+    )
+
+    assert got_user.email == email
+
+
+@pytest.mark.asyncio
+async def test_create_user(database_service: UsersDatabaseService):
+    session = MagicMock()
+    user_id = uuid.uuid4()
+    email = "test@gmail.com"
+
+    user = await database_service.create_user(
+        session=session,
+        user_id=user_id,
+        email=email,
+    )
+
+    assert user.id == user_id
+    assert user.email == email
+
+
+@pytest.mark.asyncio
+async def test_get_or_create_user(database_service: UsersDatabaseService):
+    session = MagicMock()
+    user_id = uuid.uuid4()
+    email = "test@gmail.com"
+
+    created_user = User(id=user_id, email=email)
+    database_service.get_user = AsyncMock(return_value=created_user)
+    database_service.create_user = AsyncMock(return_value=created_user)
+
+    user = await database_service.get_or_create_user(
+        session=session,
+        user_id=user_id,
+        email=email,
+    )
+
+    assert user.id is user_id
+    assert user.email == email
