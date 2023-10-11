7e1fa566ec91c17d350526e2eb88d5bfdb6166db
Oleg Yurchik <oleg@yurchik.space>
2023-10-11 21:41:44 +0300

    task/93 update cli interfaces

--- sapphire/email/cli.py
+++ sapphire/email/cli.py
@@ -3,21 +3,12 @@ import asyncio
 import typer
 from loguru import logger
 
-from . import broker, sender
-from .service import get_service
-from .settings import EmailSettings, get_settings
+from .settings import get_settings
 
 
 @logger.catch
 def run(ctx: typer.Context):
-    settings: EmailSettings = ctx.obj["settings"]
-
-    loop = asyncio.get_event_loop()
-    sender_service = sender.get_service(settings=settings)
-    broker_service = broker.get_service(loop=loop, sender=sender_service, settings=settings)
-    email_service = get_service(broker=broker_service, sender=sender_service)
-
-    loop.run_until_complete(email_service.run())
+    pass
 
 
 def settings_callback(ctx: typer.Context):
@@ -30,6 +21,5 @@ def get_cli() -> typer.Typer:
 
     cli.callback()(settings_callback)
     cli.command(name="run")(run)
-    cli.add_typer(broker.get_cli(), name="broker")
 
     return cli
