8cf4d87d48a1805a77541505506344ebc4e08e3f
matve <matveytulaevm.ama@gmail.com>
2023-10-12 17:27:46 +0300

    task 94, fixed comments, not tested

--- sapphire/users/api/v1beta/rest/handlers.py
+++ sapphire/users/api/v1beta/rest/handlers.py
@@ -1,7 +1,10 @@
 import uuid
 
 import fastapi
+from fastapi import Depends
 
+from sapphire.common.api.jwt.depends import get_user_id
+from sapphire.users.database.models import User
 from sapphire.users.database.service import UsersDatabaseService
 
 from .schemas import UserUpdateRequest, UserUpdateResponce
@@ -10,21 +13,21 @@ app = fastapi.FastAPI()
 
 
 async def update(request: fastapi.Request,
-                 user_id: uuid.UUID,
-                 user: UserUpdateRequest) -> UserUpdateResponce:
+                 user_id: uuid.UUID = Depends(get_user_id),
+                 user: UserUpdateRequest = fastapi.Body(embed=False)) -> UserUpdateResponce:
     database_service: UsersDatabaseService = request.app.service.database
 
     if user.user_id != user_id:
         raise fastapi.HTTPException(
-            status_code=fastapi.status.HTTP_400_BAD_REQUEST,
+            status_code=fastapi.status.HTTP_403_FORBIDDENT,
             detail="must be your user id",
         )
 
     async with database_service.transaction() as session:
-        user.db = await database_service.update_user(
+        user_db = await database_service.update_user(
+            user=User.id,
             session=session,
-            user_id=user.user_id,
             first_name=user.first_name,
             last_name=user.last_name,
         )
-    return UserUpdateResponce.model_validate(user.db)
+    return UserUpdateResponce.model_validate(user_db)
