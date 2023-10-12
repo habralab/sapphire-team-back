248124e2e9207bc07a5d8452213b8aefa07ba578
matve <matveytulaevm.ama@gmail.com>
2023-10-12 17:32:48 +0300

    task 94, fixed comments#2, not tested

--- sapphire/users/api/v1beta/router.py
+++ sapphire/users/api/v1beta/router.py
@@ -6,4 +6,4 @@ router = fastapi.APIRouter()
 
 router.add_api_route(methods=["GET"], path="/health", endpoint=health.health)
 router.include_router(oauth2.router, prefix="/oauth2")
-router.include_router(rest.router, prefix="/")
+router.include_router(rest.router)
