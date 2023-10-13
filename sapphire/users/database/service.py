f85191c1967c3baa6089141ee82a1645f1320f2c
YehorI <skylainer99@gmail.com>
2023-10-13 17:13:03 +0300

    task/63 create_profile fix

--- sapphire/users/database/service.py
+++ sapphire/users/database/service.py
@@ -39,7 +39,7 @@ class UsersDatabaseService(BaseDatabaseService):
             id=user_id, email=email, first_name=first_name, last_name=last_name, avatar=avatar
         )
         session.add(user)
-        self.create_profile(user)
+        self.create_profile(user=user, about=None)
 
         return user
 
