1ab3c19d692f0d893759e07e431014c77e8a30a9
YehorI <skylainer99@gmail.com>
2023-10-13 17:02:41 +0300

    task/63 create_profile in create_user + tests update + fix

--- sapphire/users/database/service.py
+++ sapphire/users/database/service.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from sapphire.common.database.service import BaseDatabaseService
-from sapphire.users.database.models import User
+from sapphire.users.database.models import User, Profile
 from sapphire.users.settings import UsersSettings
 
 from .models import User
@@ -20,11 +20,11 @@ class UsersDatabaseService(BaseDatabaseService):
         session: AsyncSession,
         email: str,
     ) -> User | None:
-        user = session().execute(
+        user = await session.execute(
             select(User).where(User.email == email)
-        ).first()
+        )
 
-        return user
+        return user.first()
 
     async def create_user(
         self,
@@ -39,6 +39,7 @@ class UsersDatabaseService(BaseDatabaseService):
             id=user_id, email=email, first_name=first_name, last_name=last_name, avatar=avatar
         )
         session.add(user)
+        self.create_profile(user)
 
         return user
 
@@ -60,9 +61,22 @@ class UsersDatabaseService(BaseDatabaseService):
                 session=session,
                 user_id=user_id, email=email
             )
-            return user
 
         return user
+
+    async def create_profile(
+        self,
+        session: AsyncSession,
+        user: User,
+        about: str | None,
+    ) -> Profile:
+        profile = Profile(
+            user_id=user.id,
+            about=about
+            )
+        session.add(profile)
+
+        return profile
 
 
 def get_service(settings: UsersSettings) -> UsersDatabaseService:
