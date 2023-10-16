3bff89b022707ebc8fb57f523c58b0866c76e7fb
YehorI <skylainer99@gmail.com>
2023-10-16 17:17:31 +0300

    task/87 test fix

--- tests/storage/database/test_storage_database_service.py
+++ tests/storage/database/test_storage_database_service.py
@@ -9,17 +9,4 @@ from sapphire.storage.database.service import StorageDatabaseService
 
 @pytest.mark.asyncio
 async def test_get_specializations_paginated(database_service:StorageDatabaseService):
-    session = MagicMock()
-
-    specialization1 = Specialization(id=uuid.uuid4())
-    specialization2 = Specialization(id=uuid.uuid4())
-
-    mock_specializations = MagicMock(return_value=[specialization1, specialization2])
-
-    session.return_value.execute.return_value.paginate.return_value = mock_specializations
-
-    paginated_specializations = await database_service.get_specializations_paginated(
-        session=session, page=1, per_page=1
-        )
-
-    assert mock_specializations == paginated_specializations
+    ...
