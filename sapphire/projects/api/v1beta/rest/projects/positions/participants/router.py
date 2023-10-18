f64be859f12c08032032904ba3ecc2531ee1968e
vanvanich9 <vanvanich9@gmail.com>
2023-10-18 09:36:34 +0200

    task/90 add update participant status handler

--- sapphire/projects/api/v1beta/rest/projects/positions/participants/router.py
+++ sapphire/projects/api/v1beta/rest/projects/positions/participants/router.py
@@ -5,8 +5,8 @@ from . import handlers
 router = fastapi.APIRouter()
 
 router.add_api_route(
-    path="/", methods=["POST"], endpoint=handlers.create_request_participate
+    path="/", methods=["POST"], endpoint=handlers.create_participant
 )
 router.add_api_route(
-    path="/{participant_id}", methods=["DELETE"], endpoint=handlers.remove_request_participate
+    path="/{participant_id}", methods=["POST"], endpoint=handlers.update_participant
 )
