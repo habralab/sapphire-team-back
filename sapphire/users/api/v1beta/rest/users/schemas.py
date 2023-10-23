5e9865fb22a3748dbc3c7883aca8a8dea57a310e
Oleg Yurchik <oleg@yurchik.space>
2023-10-23 14:21:19 +0300

    task/117 add tests + any fixes

--- sapphire/users/api/v1beta/rest/users/schemas.py
+++ sapphire/users/api/v1beta/rest/users/schemas.py
@@ -7,16 +7,18 @@ from sapphire.users.database.models import User
 
 
 class UserUpdateRequest(BaseModel):
-    first_name: str
-    last_name: str
-    main_specialization_id: uuid.UUID
-    secondary_specialization_id: uuid.UUID
+    first_name: str | None
+    last_name: str | None
+    about: str | None
+    main_specialization_id: uuid.UUID | None
+    secondary_specialization_id: uuid.UUID | None
 
 
 class UserResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: uuid.UUID
+    email: EmailStr | None
     first_name: str | None
     last_name: str | None
     about: str | None
@@ -26,29 +28,10 @@ class UserResponse(BaseModel):
     created_at: datetime
 
     @classmethod
-    def from_db_model(cls, user: User) -> "UserResponse":
+    def from_db_model(cls, user: User, with_email: bool = True) -> "UserResponse":
         return cls(
             id=user.id,
-            first_name=user.first_name,
-            last_name=user.last_name,
-            about=user.profile.about,
-            main_specialization_id=user.profile.main_specialization_id,
-            secondary_specialization_id=user.profile.secondary_specialization_id,
-            created_at=user.created_at,
-            updated_at=user.updated_at,
-        )
-
-
-class UserFullResponse(UserResponse):
-    model_config = ConfigDict(from_attributes=True)
-
-    email: EmailStr
-
-    @classmethod
-    def from_db_model(cls, user: User) -> "UserFullResponse":
-        return cls(
-            id=user.id,
-            email=user.email,
+            email=user.email if with_email else None,
             first_name=user.first_name,
             last_name=user.last_name,
             about=user.profile.about,
--- sapphire/users/database/models.py
+++ sapphire/users/database/models.py
@@ -20,8 +20,9 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
 
-    profile: Mapped["Profile"] = relationship("Profile", back_populates="user")
-    skills: Mapped[list["UserSkill"]] = relationship("UserSkill", back_populates="user")
+    profile: Mapped["Profile"] = relationship("Profile", back_populates="user", lazy="immediate")
+    skills: Mapped[list["UserSkill"]] = relationship("UserSkill", back_populates="user",
+                                                     lazy="immediate")
 
 
 class Profile(Base):
@@ -34,7 +35,7 @@ class Profile(Base):
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
 
-    user: Mapped[User] = relationship(User, back_populates="profile")
+    user: Mapped[User] = relationship(User, back_populates="profile", lazy="immediate")
 
 
 class UserSkill(Base):
@@ -45,4 +46,4 @@ class UserSkill(Base):
     created_at: Mapped[datetime] = mapped_column(default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.now, onupdate=datetime.now)
 
-    user: Mapped[User] = relationship(User, back_populates="skills")
+    user: Mapped[User] = relationship(User, back_populates="immediate")
