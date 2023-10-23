3b289fa6ea22501df63b0840087191c35c003f76
matve <matveytulaevm.ama@gmail.com>
2023-10-23 15:18:17 +0300

    task 103/ hotfix

--- sapphire/users/api/v1beta/rest/users/router.py
+++ sapphire/users/api/v1beta/rest/users/router.py
@@ -7,9 +7,9 @@ router = fastapi.APIRouter()
 router.add_api_route(path="/{user_id}", methods=["GET"], endpoint=handlers.get_user)
 router.add_api_route(path="/{user_id}", methods=["POST"], endpoint=handlers.update_user)
 router.add_api_route(path="/{user_id}/avatar", methods=["GET"], endpoint=handlers.get_user_avatar)
-router.add_api_route(path="/{user_id}/skills", methods=["POST"],
-                     endpoint=handlers.update_user_skills)
 router.add_api_route(path="/{user_id}/avatar", methods=["POST"],
                      endpoint=handlers.upload_user_avatar)
 router.add_api_route(path="/{user_id}/avatar", methods=["DELETE"],
                      endpoint=handlers.delete_user_avatar)
+router.add_api_route(path="/{user_id}/skills", methods=["POST"],
+                     endpoint=handlers.update_user_skills)
