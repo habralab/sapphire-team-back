ad70a63f5987771bd8b9ae3fa7318a41791c905f
Oleg Yurchik <oleg@yurchik.space>
2023-10-24 21:15:53 +0300

    task/120 fix tests

--- sapphire/storage/api/v1beta/rest/specializations/router.py
+++ sapphire/storage/api/v1beta/rest/specializations/router.py
@@ -4,4 +4,4 @@ from . import handlers
 
 router = fastapi.APIRouter()
 
-router.add_api_route(path="", endpoint=handlers.get_specializations)
+router.add_api_route(path="/", endpoint=handlers.get_specializations)
