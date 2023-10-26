965ca4bb0d35ebf277f10ec6b9237f4cc7f79363
Oleg Yurchik <oleg@yurchik.space>
2023-10-26 13:40:38 +0300

    task/119 hotfix: add cli commands for migrations: list and rollback

--- sapphire/projects/database/cli.py
+++ sapphire/projects/database/cli.py
@@ -1,75 +1,7 @@
-import asyncio
-from typing import Optional
-
 import typer
-from loguru import logger
-from rich.console import Console
-from rich.table import Table
 
-from sapphire.common.database.service import FixtureFormatEnum
-
+from sapphire.common.database.cli import get_fixtures_cli, get_migrations_cli
 from .service import get_service
-
-
-@logger.catch
-def migrate(ctx: typer.Context):
-    database_service = ctx.obj["database"]
-    database_service.migrate()
-
-
-@logger.catch
-def create(ctx: typer.Context,
-           message: Optional[str] = typer.Option(
-               None, "-m", "--message",
-               help="Migration short message",
-           )):
-    database_service = ctx.obj["database"]
-    database_service.create_migration(message=message)
-
-
-def get_migration_cli() -> typer.Typer:
-    cli = typer.Typer()
-
-    cli.command(name="migrate")(migrate)
-    cli.command(name="create")(create)
-
-    return cli
-
-
-@logger.catch
-def fixtures_list(ctx: typer.Context):
-    database_service = ctx.obj["database"]
-
-    fixtures = database_service.get_fixtures()
-
-    table = Table("Name", "Format")
-    for fixture in fixtures:
-        table.add_row(fixture.name, fixture.format.value)
-    Console().print(table)
-
-
-@logger.catch
-def apply_fixture(
-    ctx: typer.Context,
-    name: str = typer.Argument(..., help="Fixture name"),
-    fixture_format: FixtureFormatEnum = typer.Option(
-        FixtureFormatEnum.YAML,
-        "-f", "--format",
-        help="Fixture format/extension",
-    ),
-):
-    database_service = ctx.obj["database"]
-
-    asyncio.run(database_service.apply_fixture(name=name, fixture_format=fixture_format))
-
-
-def get_fixtures_cli() -> typer.Typer:
-    cli = typer.Typer()
-
-    cli.command(name="list")(fixtures_list)
-    cli.command(name="apply")(apply_fixture)
-
-    return cli
 
 
 def service_callback(ctx: typer.Context):
@@ -84,6 +16,6 @@ def get_cli() -> typer.Typer:
 
     cli.callback()(service_callback)
     cli.add_typer(get_fixtures_cli(), name="fixtures")
-    cli.add_typer(get_migration_cli(), name="migrations")
+    cli.add_typer(get_migrations_cli(), name="migrations")
 
     return cli
