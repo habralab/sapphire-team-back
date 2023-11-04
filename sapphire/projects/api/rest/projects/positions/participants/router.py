0e6e80067c73c7df64f21b381965644589283345
Oleg Yurchik <oleg@yurchik.space>
2023-11-04 09:18:44 +0300

    task/135 implement handler for getting participant

--- sapphire/projects/api/rest/projects/positions/participants/router.py
+++ sapphire/projects/api/rest/projects/positions/participants/router.py
@@ -5,8 +5,11 @@ from . import handlers
 router = fastapi.APIRouter()
 
 router.add_api_route(
-    path="/", methods=["POST"], endpoint=handlers.create_participant
+    path="/", methods=["POST"], endpoint=handlers.create_participant,
 )
 router.add_api_route(
-    path="/{participant_id}", methods=["POST"], endpoint=handlers.update_participant
+    path="/{participant_id}", methods=["GET"], endpoint=handlers.get_participant,
+)
+router.add_api_route(
+    path="/{participant_id}", methods=["POST"], endpoint=handlers.update_participant,
 )
