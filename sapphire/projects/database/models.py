b8d2fb477332a20d1014fdcd8aa0ec4c9090975f
Oleg Yurchik <oleg@yurchik.space>
2023-11-09 23:12:56 +0300

    task/152 add indexes to projects db tables

--- sapphire/projects/database/models.py
+++ sapphire/projects/database/models.py
@@ -2,7 +2,7 @@ import enum
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Enum, ForeignKey
+from sqlalchemy import Enum, ForeignKey, Index
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -38,10 +38,14 @@ class Project(Base):
     history: Mapped[list["ProjectHistory"]] = relationship(
         back_populates="project",
         order_by="desc(ProjectHistory.created_at)",
-        lazy="joined",
+        lazy=False,
     )
-    positions: Mapped[list["Position"]] = relationship(back_populates="project", lazy="joined")
-    reviews: Mapped[list["Review"]] = relationship(back_populates="project", lazy="joined")
+    positions: Mapped[list["Position"]] = relationship(back_populates="project", lazy=False)
+    reviews: Mapped[list["Review"]] = relationship(back_populates="project", lazy=False)
+
+    __table_args__ = (
+        Index("projects__owner_id_idx", "owner_id", postgresql_using="hash"),
+    )
 
     @property
     def status(self):
@@ -61,57 +65,70 @@ class ProjectHistory(Base):
     __tablename__ = "projects_history"
 
     id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, primary_key=True)
-    project_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("projects.id"), primary_key=True)
+    project_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("projects.id"))
     status: Mapped[ProjectStatusEnum] = mapped_column(Enum(ProjectStatusEnum))
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
 
-    project: Mapped[Project] = relationship(back_populates="history", lazy="joined")
+    project: Mapped[Project] = relationship(back_populates="history", lazy=False)
+
+    __table_args__ = (
+        Index("projects_history__project_id_idx", "project_id", postgresql_using="hash"),
+    )
 
 
 class Position(Base):
-    __tablename__ = "project_positions"
+    __tablename__ = "positions"
 
-    id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, primary_key=True, unique=True)
-    project_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("projects.id"), primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, primary_key=True)
+    project_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("projects.id"))
     specialization_id: Mapped[uuid.UUID]
     closed_at: Mapped[datetime | None]
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    project: Mapped[Project] = relationship(back_populates="positions", lazy="joined")
+    project: Mapped[Project] = relationship(back_populates="positions", lazy=False)
     participants: Mapped[list["Participant"]] = relationship(back_populates="position",
-                                                             lazy="joined")
-    skills: Mapped[list["PositionSkill"]] = relationship(back_populates="position", lazy="joined")
+                                                             lazy=False)
+    skills: Mapped[list["PositionSkill"]] = relationship(back_populates="position", lazy=False)
+
+    __table_args__ = (
+        Index("positions__project_id_idx", "project_id", postgresql_using="hash"),
+        Index("positions__specialization_id_idx", "specialization_id",
+              postgresql_using="hash"),
+    )
 
 
 class PositionSkill(Base):
-    __tablename__ = "project_positions_skills"
+    __tablename__ = "positions_skills"
 
-    position_id: Mapped[str] = mapped_column(ForeignKey("project_positions.id"), primary_key=True)
+    position_id: Mapped[str] = mapped_column(ForeignKey("positions.id"), primary_key=True)
     skill_id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    position: Mapped[Position] = relationship(back_populates="skills", lazy="joined")
+    position: Mapped[Position] = relationship(back_populates="skills", lazy=False)
 
 
 class Participant(Base):
-    __tablename__ = "project_participants"
+    __tablename__ = "participants"
 
     id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, primary_key=True)
-    position_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("project_positions.id"),
-                                                   primary_key=True)
+    position_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("positions.id"))
     user_id: Mapped[uuid.UUID]
     status: Mapped[ParticipantStatusEnum] = mapped_column(Enum(ParticipantStatusEnum))
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow)
     joined_at: Mapped[datetime | None]
 
-    position: Mapped[Position] = relationship(back_populates="participants", lazy="joined")
+    position: Mapped[Position] = relationship(back_populates="participants", lazy=False)
 
+    __table_args__ = (
+        Index("participants__position_id_idx", "position_id", postgresql_using="hash"),
+        Index("participants__user_id_idx", "user_id", postgresql_using="hash"),
+    )
 
 class Review(Base):
-    __tablename__ = "project_reviews"
+    __tablename__ = "reviews"
 
     id: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, primary_key=True)
     project_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("projects.id"))
@@ -122,4 +139,10 @@ class Review(Base):
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     updated_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    project: Mapped[Project] = relationship(back_populates="reviews", lazy="joined")
+    project: Mapped[Project] = relationship(back_populates="reviews", lazy=False)
+
+    __table_args__ = (
+        Index("reviews__project_id_idx", "project_id", postgresql_using="hash"),
+        Index("reviews__from_user_id_idx", "from_user_id", postgresql_using="hash"),
+        Index("reviews__to_user_id_idx", "to_user_id", postgresql_using="hash"),
+    )
