7d90ac641389b8a45f56ac2cb2eff3575f5d885b
matve <matveytulaevm.ama@gmail.com>
2023-11-21 22:33:24 +0300

    task/162 get all notifications

--- autotests/clients/rest/notifications/models.py
+++ autotests/clients/rest/notifications/models.py
@@ -1,8 +1,27 @@
-from typing import Literal
+import uuid
+from datetime import datetime
+from typing import Literal, Any
 
 from pydantic import BaseModel, constr
+
+from autotests.clients.rest.models import PaginatedResponse
 
 
 class HealthResponse(BaseModel):
     name: Literal["Notifications"]
     version: constr(pattern=r"^\d+\.\d+\.\d+$")
+
+
+class NotificationModel(BaseModel):
+    id: uuid.UUID
+    type: str
+    recipient_id: uuid.UUID
+    data: dict[str, Any]
+    is_read: bool
+    created_at: datetime
+    updated_at: datetime
+
+
+
+class NotificationListResponse(PaginatedResponse):
+    data: list[NotificationModel]
--- sapphire/notifications/api/rest/router.py
+++ sapphire/notifications/api/rest/router.py
@@ -1,3 +1,8 @@
 import fastapi
+from sapphire.notifications.api.rest import handlers
+
 
 router = fastapi.APIRouter()
+
+
+router.add_route(path="/notifications", methods=["GET"], endpoint=handlers.get_notifications)
