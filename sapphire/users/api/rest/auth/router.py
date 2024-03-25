10e40d1d86cf003aa4ef7954846beab599391a2c
matve <matveytulaevm.ama@gmail.com>
2024-03-25 16:12:31 +0300

    fix + reset_password

--- sapphire/users/api/rest/auth/router.py
+++ sapphire/users/api/rest/auth/router.py
@@ -10,3 +10,4 @@ router.add_api_route(methods=["POST"], path="/signup", endpoint=handlers.sign_up
 router.add_api_route(methods=["POST"], path="/signin", endpoint=handlers.sign_in)
 router.include_router(oauth2.router, prefix="/oauth2")
 router.add_api_route(methods=["POST"], path="/change_password", endpoint=handlers.change_password)
+router.add_api_route(methods=['POST'], path="/reset_password", endpoint=handlers.reset_password)
